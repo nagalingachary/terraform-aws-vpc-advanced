b3aa0f0b6c5f95beb57b00de86d4f6bee2b7d8ed
nagalingachary <sivanagalingachary@gmail.com>
2023-10-07 22:03:54 +0530

    vpc advanced

--- main.tf
+++ main.tf
@@ -70,10 +70,10 @@ resource "aws_subnet" "database" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_internet_gateway.main.id
+  # }
     tags = merge (
     var.common_tags,
     {
@@ -81,6 +81,12 @@ resource "aws_route_table" "public" {
     },
     var.public_route_table_tags
   )
+}
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
 }
 
 resource "aws_eip" "eip" {
@@ -106,10 +112,10 @@ resource "aws_nat_gateway" "main" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main.id
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_nat_gateway.main.id
+  # }
     tags = merge (
     var.common_tags,
     {
@@ -119,13 +125,19 @@ resource "aws_route_table" "private" {
   )
 }
 
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
 resource "aws_route_table" "database" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main.id
-  }
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_nat_gateway.main.id
+  # }
     tags = merge (
     var.common_tags,
     {
@@ -134,6 +146,13 @@ resource "aws_route_table" "database" {
     var.database_route_table_tags
   )
 }
+
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
 
 resource "aws_route_table_association" "public" {
     count = length(var.public_subnet_cidr)
